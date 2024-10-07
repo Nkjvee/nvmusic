621afd07f3d011b8dc21b4f4293ab1804ebfa130
Nkjvee <niko.vuorela1@outlook.com>
2024-10-07 22:04:11 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+    function toggleMenu() {
+      var menu = document.querySelector('.menu-items');
+      menu.classList.toggle('active');
+  }
+
+  function reqSubmit() {
+    alert("Your contact request has been submitted successfully.");
+    };

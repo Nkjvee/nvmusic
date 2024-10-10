f60d9131a10d615eacb5a685711b11ab244b6d53
Nkjvee <niko.vuorela1@outlook.com>
2024-10-10 16:36:32 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,8 +1,22 @@
-    function toggleMenu() {
-      var menu = document.querySelector('.menu-items');
-      menu.classList.toggle('active');
-  }
+function toggleMenu() {
+  var menu = document.querySelector(".menu-items");
+  var icon = document.querySelector(".icon");
+  var header = document.querySelector("header");
+  var logo = document.querySelector(".nav-item");
+  var navItem = document.querySelector("nav-item");
 
-  function reqSubmit() {
-    alert("Your contact request has been submitted successfully.");
-    };
+  // Toggle the 'active' class to show/hide menu
+  menu.classList.toggle("active");
+
+  // Hide the hamburger icon and logo when the menu is active, show them when inactive
+  if (menu.classList.contains("active")) {
+    icon.style.display = "none"; // Hide icon when menu is open
+    logo.style.display = "none";
+  } else {
+    icon.style.display = "block"; // Show icon when menu is closed
+  }
+}
+
+function reqSubmit() {
+  alert("Your contact request has been submitted successfully.");
+}
